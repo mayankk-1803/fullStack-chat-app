8873dc0f7fdc89bc9d5855dc759223545d1e7153
mayank-1803 <mayankmathur183@gmail.com>
2025-07-18 15:34:01 +0530

    completed

--- Backend/routes/message.route.js
+++ Backend/routes/message.route.js
@@ -5,7 +5,7 @@ import { getMessages, getUsersForSidebar, sendMessage } from '../controllers/mes
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/chat/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
-router.get("/chat/:id", protectRoute, getMessages); // renamed from `/:id`
 
 export default router;
