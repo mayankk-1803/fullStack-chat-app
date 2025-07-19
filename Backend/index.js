cac4e108b7853764d235efc5e8c5cc320e696767
mayank-1803 <mayankmathur183@gmail.com>
2025-07-19 11:03:14 +0530

    completed

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 
-app.use(express.json());
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
